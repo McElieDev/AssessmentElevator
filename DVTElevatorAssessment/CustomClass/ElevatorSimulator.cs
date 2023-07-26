24aed0aeb04d23333b6cc76e52d4642fa8d42321
= <mcelie.mundeke@gmail.com>
2023-07-26 15:49:59 +0200

    Add features such as comments and Interfaces. update codes for review purposes

--- DVTElevatorAssessment/CustomClass/ElevatorSimulator.cs
+++ DVTElevatorAssessment/CustomClass/ElevatorSimulator.cs
@@ -7,42 +7,48 @@ using static System.Console;
 
 namespace DVTElevatorAssessment.CustomClass
 {
-    public class ElevatorSimulator
-    {
-        public readonly ElevatorController elevatorController;
+	/// <summary>
+	/// This class contains the Main method that runs the elevator and all its processes.
+	/// </summary>
+	public class ElevatorSimulator
+	{
+		public readonly ElevatorController elevatorController;
 
-        public ElevatorSimulator(int elevatorCount, int floorCount, int elevatorCapacity, int elevatorWeightLimit)
-        {
-            elevatorController = new ElevatorController(elevatorCount, floorCount, elevatorCapacity, elevatorWeightLimit);
-        }
+	   public ElevatorSimulator(int elevatorCount, int floorCount, int elevatorCapacity, int elevatorWeightLimit)
+		{
+			elevatorController = new ElevatorController(elevatorCount, floorCount, elevatorCapacity, elevatorWeightLimit);
+		}
 
-        public void Run()
-        {
-            while (true)
-            {
-                WriteLine("Enter Floor number to call the elevator (1 to 5), or 0 to exit:");
-                if (!int.TryParse(ReadLine(), out int floorNumber) || (floorNumber < 0 || floorNumber > elevatorController.FloorCount))
-                {
-                    WriteLine("Invalid value! Please enter a valid floor number.");
-                    continue;
-                }
+		/// <summary>
+        /// The Main Method or Run Method to start the Elevator Simulator
+        /// </summary>
+        /// <param name="args"></param>
+		public void Main(string[] args)
+		{
+		   while (true)
+			{
+				Console.WriteLine("Enter the floor number to call the elevator (1 to 5), or 0 to exit:");
+				if (!int.TryParse(Console.ReadLine(), out int floorNumber) || (floorNumber < 0 || floorNumber > elevatorController.FloorCount))
+				{
+					Console.WriteLine("Invalid input! Please enter a valid floor number.");
+					continue;
+				}
 
-                if (floorNumber == 0)
-                    break;
+				if (floorNumber == 0)
+					break;
 
-                WriteLine($"Enter Number of people waiting on floor {floorNumber}:");
-                if (!int.TryParse(ReadLine(), out int peopleCount) || peopleCount > 10)
-                {
-                    WriteLine("Invalid value! Please enter a valid number of people less than 10 (people per Elevator).");
-                    continue;
-                }
+				Console.WriteLine($"Enter the number of people waiting on floor {floorNumber}:");
+				if (!int.TryParse(Console.ReadLine(), out int peopleCount) || peopleCount < 0)
+				{
+					Console.WriteLine("Invalid input! Please enter a valid number of people.");
+					continue;
+				}
 
-                elevatorController.SetPeopleWaiting(floorNumber, peopleCount);
-                elevatorController.CallElevator(floorNumber);
+				elevatorController.CallElevator(floorNumber, peopleCount);
 
-                WriteLine("\nElevator Status:");
-                elevatorController.DisplayElevatorStatus();
-            }
-        }
-    }
+				Console.WriteLine("\nElevator Status:");
+				elevatorController.DisplayElevatorStatus();
+			}
+		}
+	}
 }
--- DVTElevatorAssessment/CustomClass/Floor.cs
+++ DVTElevatorAssessment/CustomClass/Floor.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace DVTElevatorAssessment.CustomClass
 {
-    public class Floor
-    {
-        public int FloorNumber { get; }
-        public int PeopleWaiting { get; set; }
+	/// <summary>
+    /// This class represents a floor with its floor number. It is a simple class with no specific behavior.
+    /// </summary>
+	public class Floor
+	{
+		public int FloorNumber { get; }
 
-        public Floor(int floorNumber)
-        {
-            FloorNumber = floorNumber;
-            PeopleWaiting = 0;
-        }
-    }
+		public Floor(int floorNumber)
+		{
+			FloorNumber = floorNumber;
+		}
+	}
 }
